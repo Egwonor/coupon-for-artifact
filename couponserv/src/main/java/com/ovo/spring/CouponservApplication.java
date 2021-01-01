bec918f88649041f2ba2eac82e21e9dfb36bbb25
ovo <oghenehonour@gmail.com>
2021-01-01 21:57:09 +0100

    app

---
+++ couponserv/src/main/java/com/ovo/spring/CouponservApplication.java
@@ -0,0 +1,13 @@
+package com.ovo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponservApplication.class, args);
+	}
+
+}
